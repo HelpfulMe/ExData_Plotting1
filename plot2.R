b28953d06384f0214593b7dbafdf0fab75b54773
Becca <beccasmithusa@gmail.com>
2016-08-27 00:41:45 -0400

    Plot 2
    Finished Plot 2!

--- plot2.R
+++ plot2.R
@@ -1,4 +1,66 @@
-##FORMAT
-#change dates to date format
+##LOAD DATA
+##For this code to work please:
+## - download the data zip file from 
+##    https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+## - extract the data from the zip file
+## - set your working directory to the location of the data
+## - data file should be named "household_power_consumption.txt"
 
-#change time to time format
+#_______________________________________________________________________
+
+##READ DATA
+
+#set the classes of the data we want to read in 
+#to help save memory and increase speed
+class <- c("character", "character", "numeric", "numeric", "numeric", 
+           "numeric", "numeric", "numeric", "numeric")
+
+##to read in the ENTIRE dataset and then subset the relevant dates:
+##(commented out to allow reading in only the relevant records)
+##I ran this code to learn the correct row numbers to import.  The uncommented code
+##imports only the two dates we are working with
+
+#data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", 
+#                   na.strings = "?", colClasses = class, 
+#                   stringsAsFactors = FALSE)
+
+##subset data to only Feb 1, 2007 and Feb 2, 2007
+#datesdata <- subset(data, Date == "1/2/2007" | Date == "2/2/2007")
+
+mydata <- read.table("household_power_consumption.txt", header = FALSE, sep = ";", 
+                     skip = 66637, nrows = 2880, na.strings = "?", colClasses = class, 
+                     stringsAsFactors = FALSE)
+#data has meaningless column names (V1, V2, etc), give meaningful column names
+mycolnames <- c("Date", "Time", "Global_active_power", "Global_reactive_power", 
+                "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", 
+                "Sub_metering_3")
+names(mydata) <- mycolnames
+
+#create a new column with date and time as POSIXct format
+mydata$newdate <- with(mydata, as.POSIXct(paste(Date, Time), format="%d/%m/%Y %H:%M:%S"))
+
+
+#mydata$Date <- as.Date(mydata$Date , "%d/%m/%Y")
+
+#and times to be time
+#_______________________________________________________________________
+
+##CREATE EXPLORATORY CHART
+
+#using the base package
+
+plot(Global_active_power ~ newdate, data=mydata, type="l", xlab = "", 
+     ylab = "Global Active Power (kilowatts)")
+
+
+#_______________________________________________________________________
+
+##SAVE PLOT
+#save it to a PNG file with a width of 480 pixels and a height of 480 pixels
+
+
+## Copy my plot to a PNG file
+dev.copy(png, file = "plot2.png", width = 480, height = 480, units = "px")
+
+## Don't forget to close the PNG device!
+dev.off()
